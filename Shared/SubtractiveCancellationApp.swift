53bc62de84103a85ae13dd57b447e988526a0b0b
Spencer Lee <slee216@hawk.iit.edu>
2021-02-05 11:26:22 -0600

    Initial Commit

---
+++ Shared/SubtractiveCancellationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubtractiveCancellationApp.swift
+//  Shared
+//
+//  Created by Spencer Lee on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SubtractiveCancellationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
